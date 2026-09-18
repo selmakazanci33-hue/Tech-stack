1fcc90dd7722cfe59087decbc53605a7c9dda85e
selmakazanci33-hue <selmakazanci33@gmail.com>
2026-09-18 17:21:49 -0400

    Update 917.sql

--- Script/917.sql
+++ Script/917.sql
@@ -5,9 +5,10 @@ FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'dbo'
   AND TABLE_NAME = 'inbound_automation'
   AND (
-       COLUMN_NAME LIKE '%enrollee%'
-       OR COLUMN_NAME LIKE '%member%'
-       OR COLUMN_NAME LIKE '%indiv%'
-       OR COLUMN_NAME LIKE '%subscriber%'
-  )
+        COLUMN_NAME LIKE '%member%'
+        OR COLUMN_NAME LIKE '%enrollee%'
+        OR COLUMN_NAME LIKE '%indiv%'
+        OR COLUMN_NAME LIKE '%policy%'
+        OR COLUMN_NAME LIKE '%subscriber%'
+      )
 ORDER BY ORDINAL_POSITION;
