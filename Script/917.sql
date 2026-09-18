2cddf3b043c068537ba487098dc5ba5545b84f13
selmakazanci33-hue <selmakazanci33@gmail.com>
2026-09-18 17:32:29 -0400

    Update 917.sql

--- Script/917.sql
+++ Script/917.sql
@@ -1,14 +1,31 @@
+WITH InboundPolicies AS
+(
+    SELECT DISTINCT
+        i.issuer,
+        i.health_coverage_policy_no AS inbound_policy_id
+    FROM dbo.inbound_automation i
+    WHERE i.coverage_year = 2026
+      AND i.health_coverage_policy_no IS NOT NULL
+),
+
+EnrollmentPolicies AS
+(
+    SELECT DISTINCT
+        e.enrollment_id
+    FROM dbo.Enrollments_TEST e
+    WHERE e.coverage_year = 2026
+      AND e.enrollment_id IS NOT NULL
+)
+
 SELECT
-    COLUMN_NAME,
-    DATA_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'dbo'
-  AND TABLE_NAME = 'inbound_automation'
-  AND (
-        COLUMN_NAME LIKE '%member%'
-        OR COLUMN_NAME LIKE '%enrollee%'
-        OR COLUMN_NAME LIKE '%indiv%'
-        OR COLUMN_NAME LIKE '%policy%'
-        OR COLUMN_NAME LIKE '%subscriber%'
-      )
-ORDER BY ORDINAL_POSITION;
+    i.issuer,
+    COUNT(*) AS Missing_Policy_Count
+FROM InboundPolicies i
+WHERE NOT EXISTS
+(
+    SELECT 1
+    FROM EnrollmentPolicies e
+    WHERE e.enrollment_id = i.inbound_policy_id
+)
+GROUP BY i.issuer
+ORDER BY Missing_Policy_Count DESC;
