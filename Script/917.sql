23fcaaef0cde4bb8e3cf304cb9b2f51e09a45ee1
selmakazanci33-hue <selmakazanci33@gmail.com>
2026-09-18 17:20:20 -0400

    Update 917.sql

--- Script/917.sql
+++ Script/917.sql
@@ -1,19 +1,13 @@
 SELECT
-    COUNT(DISTINCT CASE
-        WHEN e.enrollee_id = i.member_id
-        THEN e.enrollee_id END) AS Match_Member_ID,
-
-    COUNT(DISTINCT CASE
-        WHEN e.enrollee_id = i.exchng_assigned_enrollee_id
-        THEN e.enrollee_id END) AS Match_Exchange_Enrollee_ID,
-
-    COUNT(DISTINCT CASE
-        WHEN e.enrollee_id = i.issuer_indiv_identifier
-        THEN e.enrollee_id END) AS Match_Issuer_Individual_ID
-
-FROM dbo.Enrollments_TEST e
-JOIN dbo.inbound_automation i
-    ON e.enrollee_id = i.member_id
-    OR e.enrollee_id = i.exchng_assigned_enrollee_id
-    OR e.enrollee_id = i.issuer_indiv_identifier
-WHERE e.coverage_year = 2026;
+    COLUMN_NAME,
+    DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'dbo'
+  AND TABLE_NAME = 'inbound_automation'
+  AND (
+       COLUMN_NAME LIKE '%enrollee%'
+       OR COLUMN_NAME LIKE '%member%'
+       OR COLUMN_NAME LIKE '%indiv%'
+       OR COLUMN_NAME LIKE '%subscriber%'
+  )
+ORDER BY ORDINAL_POSITION;
